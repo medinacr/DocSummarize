ebf2a8383c361accf648e30dd3d7e88416e849a0
Cristian <medinacristian282@gmail.com>
2023-05-14 22:53:31 -0700

    dropdown

--- components/DropDown.vue
+++ components/DropDown.vue
@@ -7,33 +7,40 @@ const props = defineProps({
     selected: Number
 });
 
-const enabled = ref(false);
+let enabled = ref(false);
 const selected = ref(props.selected);
+const selectedCountry = ref(props.countries.find(c => c.id === selected.value));
 
-const selectedLanguage = computed(()=> {
-    return props.countries.filter((country)=>{
-        return country.id === selected.value
-    })[0];
-});
+function toggleDropDown() {
+    enabled.value = !enabled.value;
+}
+
+function selectCountry(country) {
+    console.log(country)
+    selectedCountry.value = country;
+    selected.value = country.id;
+    enabled.value = false;
+    // Emit a 'country-selected' event with the newly selected country
+    //emit('country-selected', selectedCountry.value)
+}
 </script>
 
 <template>
     <div class="bg-white font-bold tracking-wider px-2 py-4 flex items-center rounded-xl relative cursor-pointer"
-         @click="()=>enabled=!enabled">
+         @click="toggleDropDown">
         <div class="px-1">
-            <span class="fi px-2" :class="selectedLanguage.svg"></span>
+            <span class="fi px-2" :class="selectedCountry.svg"></span>
         </div>
-        <div class="w-20 truncate" title="English">{{ selectedLanguage.language }}</div>
+        <div class="w-20 truncate" :title="selectedCountry.language">{{ selectedCountry.language }}</div>
         <div class="pl-1 w-7"><DropDownSVG /></div>
         <div class="">
             <div v-if="enabled" class="absolute bg-white w-full top-full left-0 flex flex-col rounded-b-xl font-medium items-baseline  overflow-hidden">
-                <div v-for="country in countries" :key="country.id" class="px-4 py-3 hover:bg-stone-100 w-full">
+                <div v-for="country in countries" :key="country.id" class="px-4 py-3 hover:bg-stone-100 w-full"
+                     @click="() => selectCountry(country)">
                     <span class="fi pr-2" :class="country.svg"></span>
                     {{ country.language }}
                 </div>
             </div>
         </div>
     </div>
-
 </template>
-
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,27 @@
 <template>
     <div class="py-7 px-3 bg-stone-100 flex justify-between items-center">
 
-        <DropDown :countries="countries" :selected="sourceLanguageId" />
+        <DropDown :countries="countries" :selected="sourceLanguageId" isOpen="isOpen1" @toggle="toggleDropdown1"
+                  @country-selected="sourceLanguageId = $event.id" />
         <div>
             <div class="w-9 h-9 rounded-full p-[5.5px] inline-block bg-blue-500 text-white"><Refresh /></div>
         </div>
-        <DropDown :countries="countries" :selected="translatedLanguageId" />
+        <DropDown :countries="countries" :selected="translatedLanguageId" isOpen="isOpen2" @toggle="toggleDropdown2"
+                  @country-selected="translatedLanguageId = $event.id" />
+    </div>
+
+    <div>
+        <div class="py-7 px-3 bg-white m-4 rounded-xl flex">
+            <textarea class="w-full resize-none p-3 border-none border-transparent outline-none text-lg"></textarea>
+            <button class="h-full">
+                <svg fill="none" stroke="currentColor" stroke-width="1.5" class="w-6 h-6" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12"/>
+                </svg>
+            </button>
+        </div>
     </div>
 </template>
+
 
 <script setup>
 import { reactive, ref } from 'vue';
